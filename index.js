26fe8ee7e5aff00fa488334c2da594a291a40c5f
Kevin Defalco <kevindefalco@gmail.com>
2023-01-05 23:51:14 -0300

    part3

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require("express");
+const morgan = require('morgan');
+
 const app = express();
+
 app.use(express.json())
 
+const getBody = (req) => JSON.stringify(req.body);
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body', {
+  stream: { write: message => console.log(message.trim()) },
+  skip: (req, res) => res.statusCode < 400,
+  tokens: { body: getBody }
+}));
+
+
+
+
+// Array de objetos con las entradas de la agenda telefónica
 let persons = [
     {
       "name": "Arto Hellas",
@@ -25,15 +39,77 @@ let persons = [
     },
   ]
 
+  // Manejador de solicitudes GET para obtener todas las entradas de la agenda telefónica
   app.get("/api/persons", (request, response) => {
     response.json(persons);
   });
+
+
+  // Manejador de solicitudes GET para obtener información sobre la agenda telefónica
   app.get("/info", (request, response) => {
     const currentTime = new Date();
     response.send(`<p>Phonebook has info for ${persons.length} people </p>
     <p> ${currentTime} </p>
     `);
   });
+
+
+  // Manejador de solicitudes GET para obtener una entrada de la agenda telefónica por ID
+  app.get("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id);
+    const person = persons.find((person) => person.id === id);
+    if (person) {
+      response.json(person);
+    } else {
+      response.status(404).end();
+    }
+  });
+
+  // Función para generar un ID único para una nueva entrada de la agenda telefónica
+  const generateId = () => {
+    const maxId = persons.length > 0
+      ? Math.max(...persons.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+
+  app.post("/api/persons", (request, response) => {
+    const body = request.body;
+  
+    if (!body.name || !body.number) {
+      return response.status(400).json({ 
+        error: "name or number is missing"
+      });
+    }
+
+    if (persons.find((person) => person.name === body.name)) {
+      return response.status(400).json({ 
+        error: "name must be unique"
+      });
+    }
+  
+    const person = {
+      name: body.name,
+      number: body.number,
+      id: generateId()
+    };
+  
+    persons = persons.concat(person);
+  
+    response.json(person);
+  });
+
+  app.delete("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id);
+    persons = persons.filter((person) => person.id !== id);
+    response.status(204).end();
+  });
+
+  // const unknownEndpoint = (request, response) => {
+  //   response.status(404).send({ error: 'unknown endpoint' })
+  // }
+  
+  // app.use(unknownEndpoint)
 
 
 const PORT = 3001;
